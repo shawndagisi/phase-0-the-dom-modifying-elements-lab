1665f22c255ff5062fdc52e294d31cd627bf4cdf
shawn <shawn.dagisi@student.moringaschool.com>
2023-10-19 09:51:17 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code here!
+const newHeader = document.createElement("h1");
+
+
+
+newHeader.id = "victory";
+
+
+newHeader.textContent = "Shawn is the champion";
+
+
+const mainElement = document.getElementById("main");
+
+
+if (mainElement) {
+  mainElement.remove();
+}
+
+const existingHeader = document.getElementById("main");
+
+if (existingHeader) {
+  existingHeader.replaceWith(newHeader);
+}
